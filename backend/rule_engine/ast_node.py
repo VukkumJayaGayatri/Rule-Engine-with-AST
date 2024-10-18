eb8013f049e918d5e9d025e5742c5887f3768de3
Vukkum Jaya Gayatri <99681807+VukkumJayaGayatri@users.noreply.github.com>
2024-10-19 02:05:51 +0530

    Delete backend directory

--- backend/rule_engine/ast_node.py
+++
@@ -1,9 +0,0 @@
-class ASTNode:
-    def __init__(self, node_type, value=None, left=None, right=None):
-        self.type = node_type
-        self.value = value
-        self.left = left
-        self.right = right
-
-    def __repr__(self):
-        return f"ASTNode({self.type}, {self.value}, {self.left}, {self.right})"
